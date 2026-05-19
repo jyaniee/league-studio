3e89a0f880f3eae7689bc4628385398475811fa7
seoyeon0225 <seoyeon040225@gmail.com>
2026-05-20 03:47:18 +0900

    Refactor MainBar to use GameData type and simplify props

--- apps/overlay/src/components/scoreboard/MainBar.tsx
+++ apps/overlay/src/components/scoreboard/MainBar.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import TeamHeader from './TeamHeader';
 import TeamStats from './TeamStats';
 import CenterScore from './CenterScore';
+import { GameData } from './TopScoreboard';
 
-export default function MainBar({ gameData, handleUpdateKill }: any) {
+type MainBarProps = {
+  gameData: GameData;
+};
+
+export default function MainBar({ gameData }: MainBarProps) {
   return (
     <div style={{ width: '100%', maxWidth: '1300px', height: '80px', display: 'flex', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.85)', boxShadow: '0 4px 20px rgba(0, 0, 0, 0.5)', padding: '0 20px' }}>
-      <TeamHeader name={gameData.blueTeam.name} logo={gameData.blueTeam.logo} align="left" />
-      <TeamStats kills={gameData.blueTeam.kills} towers={gameData.blueTeam.towers} gold={gameData.blueTeam.gold} goldDiff={gameData.blueTeam.goldDiff} align="left" />
-      <CenterScore blueKills={gameData.blueTeam.kills} redKills={gameData.redTeam.kills} updateKill={handleUpdateKill} />
-      <TeamStats kills={gameData.redTeam.kills} towers={gameData.redTeam.towers} gold={gameData.redTeam.gold} goldDiff={gameData.redTeam.goldDiff} align="right" />
-      <TeamHeader name={gameData.redTeam.name} logo={gameData.redTeam.logo} align="right" />
+      <TeamHeader name={gameData.blueTeam.name} logo={gameData.blueTeam.logo} side="blue" />
+      <TeamStats kills={gameData.blueTeam.kills} towers={gameData.blueTeam.towers} gold={gameData.blueTeam.gold} goldDiff={gameData.blueTeam.goldDiff} side="blue" />
+      <CenterScore blueKills={gameData.blueTeam.kills} redKills={gameData.redTeam.kills} />
+      <TeamStats kills={gameData.redTeam.kills} towers={gameData.redTeam.towers} gold={gameData.redTeam.gold} goldDiff={gameData.redTeam.goldDiff} side="red" />
+      <TeamHeader name={gameData.redTeam.name} logo={gameData.redTeam.logo} side="red" />
     </div>
   );
 }
